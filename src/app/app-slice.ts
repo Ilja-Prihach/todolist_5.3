577100960759db1add79620382069952c86b84a4
Ilja-Prihach <piratehnik9508@gmail.com>
2025-10-20 19:12:21 +0300

    add skeletom in first loading from todolist

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,5 +1,7 @@
 import type { RequestStatus } from "@/common/types"
-import { createSlice, isFulfilled, isPending, isRejected } from "@reduxjs/toolkit"
+import { createSlice, isPending } from "@reduxjs/toolkit"
+import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
+import { tasksApi } from "@/features/todolists/api/tasksApi.ts"
 
 export const appSlice = createSlice({
   name: "app",
@@ -31,14 +33,23 @@ export const appSlice = createSlice({
   }),
     extraReducers: (builder) => {
       builder
-        .addMatcher(isPending, (state) => {
+        // .addMatcher(isPending, (state) => {
+        //   state.status = "loading"
+        // })
+        // .addMatcher(isFulfilled, (state) => {
+        //   state.status = "succeeded"
+        // })
+        // .addMatcher(isRejected, (state) => {
+        //   state.status = "failed"
+        // })
+        .addMatcher(isPending, (state, action) => {
+          if (
+            todolistsApi.endpoints.getTodolists.matchPending(action) ||
+            tasksApi.endpoints.getTasks.matchPending(action)
+          ) {
+            return
+          }
           state.status = "loading"
-        })
-        .addMatcher(isFulfilled, (state) => {
-          state.status = "succeeded"
-        })
-        .addMatcher(isRejected, (state) => {
-          state.status = "failed"
         })
     },
 })
